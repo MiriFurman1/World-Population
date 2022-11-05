924d921c3e27b3d73d8b30aa07eec40a17182be8
Miri <mirifurman@gmail.com>
2022-11-05 16:50:24 +0200

    05.11.22

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 
 let countries =[]
 let population=[]
-let countriesCode=[]
+let allData=[]
+let allCitiesData=[]
+let citiesData;
+let cities=[]
+let citiesPopulation=[]
+let errorMessage
 const chartDiv=document.querySelector(".chartDiv")
-
 const countriesBtnDiv=document.querySelector(".countriesBtnDiv")
 let myChart;
+let header=document.querySelector("h4")
 
-const fetchData = async (url) => {
+const fetchDataCountry = async (url) => { // func for getting data about countries in a region
     try {
       const response = await fetch(url);
       const data = await response.json();
@@ -16,74 +21,158 @@ const fetchData = async (url) => {
       console.log(e);
     }
   }; 
+// you should add headers to you request, this header is saying what format you are receiving  the data
 
-//   const getCities=async()=>{
-//     let cities=await fetchData("https://countriesnow.space/api/v0.1/countries")
-//     console.log(cities);
-//   }
-//   getCities()
-
-  let getDataOfRegion= async (region)=>{
-    let data=await fetchData(`https://restcountries.com/v3.1/region/${region}`)
-    console.log(data);
-    data.forEach(country => {
-    countries.push(country.name.common)
-    population.push(country.population)
-    countriesCode.push(country.cca2)
-    console.log(countriesCode);
-    });
-    return{
-        countries, population, countriesCode
+  const fetchDataCities = async (url,country) => { // func for getting data about cities in a country
+    try {
+      const response = await fetch(url,{
+        method:"POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            // "limit": 10,
+            "order": "asc",
+            "orderBy": "name",
+            "country": country
+        })
+      });
+      const data = await response.json();
+      return data;
+    } catch (e) {
+      console.log(e);
     }
-    
-  }
-
-countriesBtnDiv.addEventListener("click",x=>{
-    console.log(x.target.classList[0]);
-})
+  }; 
 
 
-const getDataMakeChartAfterClick = async()=>{
+
+
+ 
+
+  const getDataMakeChartAfterClick = async()=>{ //!make chart for region
     let regionsBtn=document.querySelector(".regions-btn")
-    regionsBtn.addEventListener("click",async (x) =>{
-        countries=[]
-        population=[]
+    regionsBtn.addEventListener("click",async (x) =>{ //when clicking on the region a chart will appear
+        if (x.target.classList.value!=="regions-btn"){ //if an actual button was clicked and not just the div
+            setSpinner(true)
+            countries=[] //empty the array for the next chart
+            population=[]
+            header.innerHTML=""
+            let region=x.target.classList.value
+            let results = allData.find(x=>x.region==region)
+            
+            if(results==undefined){
+            results=await getDataOfRegion(region)
+            results["region"]=region
+            allData.push(results)
+            }
+            countries=results["countries"]
+            population=results["population"]
+            makeChart(countries,population)
+            header.innerHTML=region
+            makeCountryButtons(countries)
+            setSpinner(false)
+        }
 
-
-       
-        let region=x.target.classList.value
-        console.log(x.target.classList.value);
-        let results=await getDataOfRegion(region)
-        console.log(results);
-        makeChart(countries,population)
-        makeCountryButtons(countries)
     })
 }
 
 getDataMakeChartAfterClick()
 
-const makeCountryButtons=async (countries,countriesCode)=>{
-    countriesBtnDiv.innerHTML=""
+    let getDataOfRegion= async (region)=>{ //!get data for region
+    let data=await fetchDataCountry(`https://restcountries.com/v3.1/region/${region}`) //get the data for specific region
+    console.log(data);
+    data.forEach(country => {
+    countries.push(country.name.common)
+    population.push(country.population)
+    });
+    return{
+        countries, population
+    }
+    
+  }
 
-    for(let i=0; i<=countries.length;i++){
+  const getDataForCountryAndMakeChart =async ()=>{ //!get data for the cities in the country
+    countriesBtnDiv.addEventListener("click",async(x)=>{
+        if(x.target!=countriesBtnDiv){
+            setSpinner(true)
+            cities=[]
+            citiesPopulation=[]
+            header.innerHTML=""
+            if(errorMessage!=undefined){
+                errorMessage=undefined
+                setSpinner(false)
+            }
+            let country=x.target.innerHTML
+
+            console.log(allCitiesData);
+            let citiesData = allCitiesData.find(x=>x.data[0].country==country)
+            
+            
+            if(citiesData==undefined){
+            citiesData=await fetchDataCities("https://countriesnow.space/api/v0.1/countries/population/cities/filter",country); 
+            if(!citiesData.error)
+            allCitiesData.push(citiesData)
+            }
+
+    
+        if(citiesData.error){ //if no data found, print error
+            setSpinner(false)
+            errorMessage=document.createElement("h3")
+            errorMessage.innerHTML=`oops! no data found in the API for ${country}`
+            chartDiv.appendChild(errorMessage)
+            
+        }
+        else {
+            citiesData.data.forEach((e)=>{
+                citiesPopulation.push(e.populationCounts[0].value)
+                cities.push(e.city)
+                makeChart(cities,citiesPopulation)
+                header.innerHTML=country
+                
+            })
+            
+           
+            console.log(allCitiesData);
+            setSpinner(false)
+        }
+  
+        
+    }
+    
+})}
+
+getDataForCountryAndMakeChart()
+
+
+
+
+
+const makeCountryButtons=async (countries)=>{
+    countriesBtnDiv.innerHTML=""
+    setSpinner(true)
+    for(let i=0; i<countries.length;i++){
         let country=await countries[i]
-        // let countryCode=await countriesCode[i]
         let btn=document.createElement("button")
         btn.innerHTML=country
-        // console.log(countryCode);
-        // btn.classList.add(`${countriesCode[i]}`)
         countriesBtnDiv.appendChild(btn)
     }
+    setSpinner(false)
 
-    // countries.forEach((country,i,countriesCode)=>{
-    //     let btn=document.createElement("button")
-    //     btn.innerHTML=country
-    //     console.log(countriesCode);
-    //     btn.classList.add(`${countriesCode[i]}`)
-    //     countriesBtnDiv.appendChild(btn)
-
-    // })
 }
+
+const setSpinner = (bool) => {
+    if (bool) {
+    const spinner = document.createElement('h3');
+    spinner.textContent = 'Loading';
+    chartDiv.appendChild(spinner);
+    } else {
+    const spinner = document.querySelector('h3');
+    if(spinner){
+        chartDiv.removeChild(spinner);
+    }
+    
+    }
+};
 
 const makeChart=(countries, population)=>{
     if(myChart!=undefined){
